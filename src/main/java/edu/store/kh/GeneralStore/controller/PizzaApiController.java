0d7e1fafe4422265b98c31fca9c62043e114a071
Kyungnan Kim <kht241022@gmail.com>
2025-02-03 10:49:55 +0900

    pizza api select

--- src/main/java/edu/store/kh/GeneralStore/controller/PizzaApiController.java
+++ src/main/java/edu/store/kh/GeneralStore/controller/PizzaApiController.java
@@ -1,19 +1,27 @@
 package edu.store.kh.GeneralStore.controller;
 
 
+import edu.store.kh.GeneralStore.dto.Pizza;
+import edu.store.kh.GeneralStore.service.PizzaService;
+import edu.store.kh.GeneralStore.service.PizzaServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RequestMapping("/api")
 @RestController
 public class PizzaApiController {
     // ServiceImpl -> Autowired 호출
+    @Autowired
+    private PizzaServiceImpl pizzaService;
     @GetMapping("/pizzas") //   /api/pizzas
-    public void getAllPizzas() {
-
+    public List<Pizza> selectAll() {
+        return pizzaService.selectAll();
     }
 
     @PostMapping("/pizzas") //   /api/pizzas
