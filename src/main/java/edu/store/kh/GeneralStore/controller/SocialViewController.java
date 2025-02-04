ecc62cf05128229884c226409a67ca230decfc9b
Kyungnan Kim <kht241022@gmail.com>
2025-02-04 14:50:34 +0900

    pizza api insert update uuid currentTimeMills

---
+++ src/main/java/edu/store/kh/GeneralStore/controller/SocialViewController.java
@@ -0,0 +1,14 @@
+package edu.store.kh.GeneralStore.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SocialViewController {
+
+    // 카카오 네이버 로그인 html 연결
+
+    // 카카오 회원가입 html 연결
+
+    // 네이버 회원가입 html 연결
+}
